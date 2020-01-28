ff8dcb0e0ea5a536404208f33d8fe38e57528c7e
Fabliha Hossain <fablihahossain27@gmail.com>
2020-01-28 17:30:07 -0500

    tested the initial connection

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ from application import app
 from flask import render_template, request, url_for
 
 # Defining basic route
-@app.route("/"):
+@app.route("/")
+def index():
 	return render_template("layout.html")
