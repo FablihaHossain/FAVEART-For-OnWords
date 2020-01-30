802eed2490f62207be4d54ed269711b55804a363
Fabliha Hossain <fablihahossain27@gmail.com>
2020-01-30 14:29:34 -0500

    tested the user model by getting the current data in the users table and passing to the web page

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from application import app
+from application import app, db
 from flask import render_template, request, url_for
+from models import Users, Paths, Checkpoints, Interactions
 
 # Defining basic route
 @app.route("/")
 def index():
-	return render_template("layout.html")
+	user_list = Users.query.all()
+	return render_template("layout.html", users = user_list)
