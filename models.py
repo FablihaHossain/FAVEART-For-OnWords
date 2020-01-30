5d0654e6fd38604de6e3b3b457c3ff5d33b39854
Fabliha Hossain <fablihahossain27@gmail.com>
2020-01-30 14:28:45 -0500

    small modifications to the syntax

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from application import db
 
 # User Table
-class User(db.model):
+class Users(db.Model):
 	__table_args__ = {'extend_existing': True}
 	user_id = db.Column(db.Integer, primary_key = True, unique = True)
-	firtname = db.Column(db.String(50), nullable = False)
+	firstname = db.Column(db.String(50), nullable = False)
 	lastname = db.Column(db.String(50), nullable = False)
 	email = db.Column(db.String(200), unique = True, nullable = False)
 	username = db.Column(db.String(100), unique = True, nullable = False)
@@ -13,7 +13,7 @@ class User(db.model):
 	role = db.Column(db.String(30))
 
 # Path Table
-class Path(db.model):
+class Paths(db.Model):
 	__table_args__ = {'extends_existing': True}
 	path_id = db.Column(db.Integer, primary_key = True, unique = True)
 	name = db.Column(db.String(100), nullable = True)
@@ -25,26 +25,26 @@ class Path(db.model):
 	pathmaker = db.Column(db.String(100), nullable = False)
 
 # Checkpoint Table 
-class Checkpoint(db.model):
+class Checkpoints(db.Model):
 	__table_args__ = {'extends_existing': True}
 	checkpoint_id = db.Column(db.Integer, primary_key = True, unique = True)
 	# Array of strings of text at that particular checkpoint
 	# Note: Must be maximun of 3
-	text = db.Column(db.ARRAY(db.String(100), nullable = False))
+	text = db.Column(db.ARRAY(db.String(100)), nullable = False)
 	# Array of animations to correspond with the text
 	animations = db.Column(db.ARRAY(db.String(200)), nullable = False)
 	color = db.Column(db.String(200))
 	# Latitude and Longitude
 	geolocation = db.Column(db.ARRAY(db.Float))
 
-class Interactions(db.model):
+class Interactions(db.Model):
 	__table_args__ = {'extends_existing': True}
 	interaction_id = db.Column(db.Integer, primary_key = True, unique = True)
 	path_id = db.Column(db.Integer, db.ForeignKey('path.path_id'), nullable = False)
 	checkpoint_id = db.Column(db.Integer, db.ForeignKey('checkpoint.checkpoint_id'), nullable = False)
 	# Array of user ids of those explorers that made the interaction
 	user_id = db.Column(db.ARRAY(db.Integer))
-	datetime = db.Column(db.Datetime)
+	datetime = db.Column(db.DateTime)
 
 # Credit to https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
 # Credit to https://docs.sqlalchemy.org/en/13/orm/join_conditions.html#relationship-primaryjoin
