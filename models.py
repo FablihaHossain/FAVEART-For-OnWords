68b388fae2210d0eb3e8ef37637d35d60c991c15
Fabliha Hossain <fablihahossain27@gmail.com>
2020-01-31 10:43:37 -0500

    finalized the database structure and fixed minor error in extend_existing

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class Users(db.Model):
 
 # Path Table
 class Paths(db.Model):
-	__table_args__ = {'extends_existing': True}
+	__table_args__ = {'extend_existing': True}
 	path_id = db.Column(db.Integer, primary_key = True, unique = True)
 	name = db.Column(db.String(100), nullable = True)
 	description = db.Column(db.String(1000000), nullable = False)
@@ -26,19 +26,19 @@ class Paths(db.Model):
 
 # Checkpoint Table 
 class Checkpoints(db.Model):
-	__table_args__ = {'extends_existing': True}
+	__table_args__ = {'extend_existing': True}
 	checkpoint_id = db.Column(db.Integer, primary_key = True, unique = True)
 	# Array of strings of text at that particular checkpoint
 	# Note: Must be maximun of 3
 	text = db.Column(db.ARRAY(db.String(100)), nullable = False)
 	# Array of animations to correspond with the text
 	animations = db.Column(db.ARRAY(db.String(200)), nullable = False)
-	color = db.Column(db.String(200))
+	color = db.Column(db.String(100))
 	# Latitude and Longitude
 	geolocation = db.Column(db.ARRAY(db.Float))
 
 class Interactions(db.Model):
-	__table_args__ = {'extends_existing': True}
+	__table_args__ = {'extend_existing': True}
 	interaction_id = db.Column(db.Integer, primary_key = True, unique = True)
 	path_id = db.Column(db.Integer, db.ForeignKey('path.path_id'), nullable = False)
 	checkpoint_id = db.Column(db.Integer, db.ForeignKey('checkpoint.checkpoint_id'), nullable = False)
@@ -47,5 +47,4 @@ class Interactions(db.Model):
 	datetime = db.Column(db.DateTime)
 
 # Credit to https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
-# Credit to https://docs.sqlalchemy.org/en/13/orm/join_conditions.html#relationship-primaryjoin
-# Credit to 
+# Credit to https://docs.sqlalchemy.org/en/13/orm/join_conditions.html#relationship-primaryjoin 
