75a2ad61d24ddd61836c9b24ecada11987befddf
Fabliha Hossain <fablihahossain27@gmail.com>
2020-02-01 18:47:31 -0500

    created the three other tables and tested to ensure the user can access and update the database

--- app.py
+++ app.py
@@ -5,5 +5,15 @@ from models import Users, Paths, Checkpoints, Interactions
 # Defining basic route
 @app.route("/")
 def index():
+	# Getting all users in the database
 	user_list = Users.query.all()
-	return render_template("layout.html", users = user_list)
+
+	# Getting all the paths in the database
+	paths_list = Paths.query.all()
+
+	# Getting all the checkpoints in the database
+	checkpoint_list = Checkpoints.query.all()
+
+	# Getting all the interactions in the database
+	interaction_list = Interactions.query.all()
+	return render_template("layout.html", users = user_list, paths = paths_list, checkpoints = checkpoint_list, interactions = interaction_list)
