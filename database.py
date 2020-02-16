5f7dce2ecb872a8263b4f10527a1d273c82948c7
Fabliha Hossain <fablihahossain27@gmail.com>
2020-02-15 19:58:32 -0500

    updated some functions (select, update, and delete)

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ from sqlalchemy.inspection import inspect
 from application import db, conn
 from models import Users, Paths, Checkpoints, Interactions
 from config import Config
-from sqlalchemy import text
 
 # Getting a cursor object from database connection
 cursor = conn.cursor()
@@ -39,16 +38,16 @@ class Database():
 
 		# Since the ID numbers are generated through increments, the very last object in the table is found
 		# From there, the last_id variable will be determined
-		if tablename == "Users":
+		if tablename.lower() == "users":
 			lastUser = Users.query.order_by(Users.user_id.desc()).first()
 			last_id = lastUser.user_id
-		elif tablename == "Paths":
+		elif tablename.lower() == "paths":
 			lastPath = Paths.query.order_by(Paths.path_id.desc()).first()
 			last_id = lastPath.path_id
-		elif tablename == "Checkpoints":
+		elif tablename.lower() == "checkpoints":
 			lastCheckpoint = Checkpoints.query.order_by(Checkpoints.checkpoint_id.desc()).first()
 			last_id = lastCheckpoint.checkpoint_id
-		elif tablename == "Interactions":
+		elif tablename.lower() == "interactions":
 			lastInteraction= Interactions.query.order_by(Interactions.interaction_id.desc()).first()
 			last_id = lastInteraction.interaction_id
 		else:
@@ -69,38 +68,48 @@ class Database():
 
 		# If the username and email doesn't already exist, the new user is added to the database
 		if not username_exists and not email_exists:
-			# Getting the next ID
-			nextId = Database.next_id("Users")
-			# Creating the new user
-			newUser = Users(user_id = nextId, firstname = firstname, lastname = lastname, email = email, username = username, password = password, role = role)
+				# Getting the next ID
+				nextId = Database.next_id("Users")
 
-			# Adding the new user to the database
-			db.session.add(newUser)
+				# Creating the new user
+				newUser = Users(user_id = nextId, firstname = firstname, lastname = lastname, email = email, username = username, password = password, role = role)
 
-			# Commiting the change
-			db.session.commit()
+				# Adding the new user to the database
+				db.session.add(newUser)
+
+				# Commiting the change
+				db.session.commit()
 
 	# Insert Path into Database
 	# Pathmaker cannot have two paths with the same name AND description
 	def insert_path(pathname, path_description, checkpoint_ids, interaction_ids, pathmaker, status, codes):
-		# Checking if the path already exists in the system (for the specified pathmaker)
-		query = text("SELECT * FROM paths WHERE name = '{}' AND description = '{}' AND pathmaker = '{}'".format(pathname, path_description, pathmaker))
-		exists = db.engine.execute(query).fetchone() is not None
+		try:
+			# Checking if the path already exists in the system (for the specified pathmaker)
+			query = "SELECT * FROM paths WHERE name = '%s' AND description = '%s' AND pathmaker = '%s'" % (pathname, path_description, pathmaker)
 
-		# If the path doesn't exist, it is then added to the database
-		if not exists:
-			# Getting the next path ID
-			nextID = Database.next_id("Paths")
+			# Executing the query
+			cursor.execute(query)
 
-			# Creating a new path
-			newPath = Paths(path_id = nextID, name = pathname, description = path_description, checkpoints = checkpoint_ids, interactions = interaction_ids, pathmaker = pathmaker, status = status, access_codes = codes)
+			# Finding out if it exists in the table
+			exists = cursor.fetchone() is not None
+		
+			# If the path doesn't exist, it is then added to the database
+			if not exists:
+				# Getting the next path ID
+				nextID = Database.next_id("Paths")
 
-			# Adding the new path to the database
-			db.session.add(newPath)
+				# Creating a new path
+				newPath = Paths(path_id = nextID, name = pathname, description = path_description, checkpoints = checkpoint_ids, interactions = interaction_ids, pathmaker = pathmaker, status = status, access_codes = codes)
 
-			# Commiting the change to the database
-			db.session.commit()
-	# # Insert Checkpoint into Database
+				# Adding the new path to the database
+				db.session.add(newPath)
+
+				# Commiting the change to the database
+				db.session.commit()
+		except psycopg2.DataError:
+			raise
+
+	# Insert Checkpoint into Database
 	def insert_checkpoint(text_list, animation_list, color, geolocation):
 		# Getting the next ID
 		nextId = Database.next_id("Checkpoints")
@@ -113,7 +122,8 @@ class Database():
 
 		# Commiting the change to the database
 		db.session.commit()
-	# # Insert Interaction into Database 
+	
+	# Insert Interaction into Database 
 	def insert_interaction(path_id, checkpoint_id, user_ids, currentDatetime):
 		# Getting the next ID
 		nextId = Database.next_id("Interactions")
@@ -129,37 +139,63 @@ class Database():
 
 	# Select From Table in Database
 	def select_where(tablename, pkcolumn, pk, columnName):
-		# Developing the query
-		query = text("SELECT {} FROM {} WHERE {} = '{}'".format(columnName, tablename, pkcolumn, pk))
+		try:
+			# Developing the query
+			print(tablename)
+			print(columnName)
+			print(pk)
+			print(pkcolumn)
+			query = "SELECT %s FROM %s WHERE %s = %d" % (columnName, tablename, pkcolumn, pk)
+			print("two")
 
-		# Executing the query
-		result = db.session.execute(query).fetchone()
+			# Executing the query
+			cursor.execute(query)
+			print("one")
 
-		# Reformating the result statement
-		result = str(result).strip("(',')")
+			# Getting the result
+			result = cursor.fetchone()
 
-		# Returning result
-		if result != "None":
-			return result 
-		else:
-			return "Sorry, cannot find value from table {}".format(tablename)
+			# Reformating the result statement
+			result = str(result).strip("(',')")
+			print("Getting result in the selet funcion")
+			print(result)
+
+			# Returning result
+			if result != "None":
+				return result 
+			else:
+				return "Sorry, cannot find value from table {}".format(tablename)
+		except:
+				return "Error! Invalid Entry. Please try again"
 
 	# Update Table in Database
 	def update_table(tablename, pkcolumn, pk, columnName, newValue):
-		# Developing the query
-		query = text("UPDATE {} SET {} = '{}' WHERE {} = {}".format(tablename, columnName, newValue, pkcolumn, pk))
+		try:
+			# Developing the query
+			query = "UPDATE %s SET %s = '%s' WHERE %s = %d" % (tablename, columnName, newValue, pkcolumn, pk)
 
-		# Executing the query
-		db.engine.execute(query)
+			# Executing the query
+			cursor.execute(query)
+
+			# Commiting the change
+			conn.commit()
+		except:
+			return "Error! Something went wrong"
 
 	# Delete From Table in Database 
 	# Really needs some work, especially with different types of input
 	def delete_from(tablename, pkcolumn, pk, columnName, columnValue):
-		# Developing the query
-		query = text("DELETE FROM {} WHERE {} = '{}'".format(tablename, columnName, columnValue))
+		try:
+			# Developing the query
+			query = "DELETE FROM %s WHERE %s = '%s' AND %s = %d" % (tablename, columnName, columnValue, pkcolumn, pk)
 
-		# Executing the query
-		db.engine.execute(query)
+			# Executing the query
+			cursor.execute(query)
+
+			# Commiting the change
+			conn.commit()
+		except:
+			return "Error! Something went wrong"
 
 	# Validate Login
 	def validate_login(username, password):
@@ -171,8 +207,18 @@ class Database():
 
 		# If it exists, it checks if the password given is correct
 		if exists:
+			# Getting the user id of the user
+			query = "SELECT user_id FROM users WHERE username = '%s'" % username
+			cursor.execute(query)
+			current_id = cursor.fetchone()
+			current_id = str(current_id).strip("(',')")
+			print("after getting id")
+			print(current_id)
+
 			# Getting the password from the database
-			user_password = Database.select_where("users", "username", username, "password")
+			user_password = Database.select_where("users", "user_id", current_id, "password")
+			print("getting pass in the validate function")
+			print(user_password)
 
 			# Comparing the two passwords to see if they are a match
 			if user_password == password:
