e5acf1d6f98fd4261c5309e6004520fb327c4537
Fabliha Hossain <fablihahossain27@gmail.com>
2020-02-21 14:49:13 -0500

    removed the testing code from app.py

--- app.py
+++ app.py
@@ -18,32 +18,4 @@ def index():
 	#Getting all the interactions in the database
 	interaction_list = Interactions.query.all()
 
-	# Add new user to test password hashing
-	Database.insert_user("John", "Smith", "jsmith@gmail.com", "jsmith", "mystery", "guest")
-
-	# Testing how to match hashed passwords
-	# hash_pw1 = Database.hash_password("password")
-
-	# hash_pw2 = Database.hash_password("password")
-	# decoded_hash = hash_pw1.decode('UTF-8')
-	# decoded_hash2 = hash_pw2.decode('UTF-8')
-	# print(decoded_hash)
-	# print(decoded_hash2)
-
-	# # Getting the current password hash
-	pw3 = Database.select_where("users", "user_id", 7, "password")
-	# print(pw1)
-
-	# if decoded_hash == decoded_hash2:
-	# 	valid = True
-	# else:
-	# 	valid = False
-
-	valid = Database.validate_login("jsmith", "mysTery")
-	print(valid)
-
-
-
-	# pw1_check = Database.check_hashed_passwords(hash_pw1, 'password')
-
 	return render_template("layout.html", users = user_list, paths = paths_list, checkpoints = checkpoint_list, interactions = interaction_list)
