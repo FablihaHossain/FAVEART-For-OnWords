dedfc237ebea8cb72b83d9add9b81252094158dc
Fabliha Hossain <fablihahossain27@gmail.com>
2020-02-20 21:38:10 -0500

    testing to ensure the bcrypt functions work properly;

--- app.py
+++ app.py
@@ -18,4 +18,16 @@ def index():
 	#Getting all the interactions in the database
 	interaction_list = Interactions.query.all()
 
+	# Testing the password hash function
+	# pw = Database.hash_password("passSword")
+	# pw2 = Database.hash_password("awesome")
+	# print("Password 1: %s" % pw)
+	# print("Password 2: %s" % pw2)
+
+	# # Testing the same password function
+	# password1 = Database.check_hashed_passwords(pw, 'passSword')
+	# password2 = Database.check_hashed_passwords(pw2, 'awesome')
+	# print("For password 1: %s" % password1)
+	# print("For password 2: %s" % password2)
+
 	return render_template("layout.html", users = user_list, paths = paths_list, checkpoints = checkpoint_list, interactions = interaction_list)
