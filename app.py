f5c1c8b5dbd4e80a75892d9566061aa23152e047
Fabliha Hossain <fablihahossain27@gmail.com>
2020-03-01 12:38:19 -0500

    S20SS2FAVE-62 login route can now validate the user credentials and create flash session

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from application import app, db
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, session, flash, redirect
 from models import Users, Paths, Checkpoints, Interactions
 from database import Database
 from datetime import *
@@ -21,11 +21,42 @@ def index():
 	return render_template("layout.html", users = user_list, paths = paths_list, checkpoints = checkpoint_list, interactions = interaction_list)
 
 # Login Page
-@app.route("/login")
+@app.route("/login", methods = ['GET', 'POST'])
 def login():
+	# Getting the username and password entered
+	if request.method == "POST":
+		# If either field is empty, gives an error message
+		if request.form['username'] is "" or request.form['password'] is "":
+			flash("Error! Neither field can be empty!")
+		else:
+			username = request.form['username']
+			password = request.form['password']
+
+			# Validating login
+			valid = Database.validate_login(username, password)
+
+			#If valid, creates a flash session with username and user_id
+			if valid:
+				# Getting the user_id of the current user
+				user_id = Database.get_userId(username)
+
+				# Creating a session with the user
+				session['username'] = username
+				session['user_id'] = user_id 
+
+				# Taking the user to the homepage
+				return redirect(url_for('homepage'))
+
+			else:
+				flash("Invalid Login! Please Try Again")
 	return render_template("login.html")
 
 # Register Page
-@app.route("/register")
+@app.route("/register", methods = ['GET', 'POST'])
 def register():
 	return render_template("register.html")
+
+# Home Page
+@app.route("/homepage")
+def homepage():
+	return render_template("homepage.html")
